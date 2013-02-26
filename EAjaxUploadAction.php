e0325e2322e28a6011086e60d4e6dc630301b52d
ianaré sévi <ianare.sevi@digitick.com>
2013-02-26 15:26:52 +0100

    auto format code

--- EAjaxUploadAction.php
+++ EAjaxUploadAction.php
@@ -3,17 +3,18 @@
 class EAjaxUploadAction extends CAction
 {
 
-        public function run()
-        {
-                Yii::import("ext.EAjaxUpload.qqFileUploader");
+	public function run()
+	{
+		Yii::import("ext.EAjaxUpload.qqFileUploader");
 
-                $allowedExtensions = array("jpg");
-                $sizeLimit = 1 * 1024 * 1024;
+		$allowedExtensions = array("jpg");
+		$sizeLimit = 1 * 1024 * 1024;
 
-                $uploader = new qqFileUploader($allowedExtensions, $sizeLimit);
-                $result = $uploader->handleUpload('upload/eajaxupload/');
-                // to pass data through iframe you will need to encode all html tags
-                $result=htmlspecialchars(json_encode($result), ENT_NOQUOTES);
-                echo $result;
-        }
+		$uploader = new qqFileUploader($allowedExtensions, $sizeLimit);
+		$result = $uploader->handleUpload('upload/eajaxupload/');
+		// to pass data through iframe you will need to encode all html tags
+		$result = htmlspecialchars(json_encode($result), ENT_NOQUOTES);
+		echo $result;
+	}
+
 }
