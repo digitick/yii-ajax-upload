304b7b3b562a3ec3fa2d485372faacce0174e487
ianaré sévi <ianare.sevi@digitick.com>
2013-05-21 15:53:16 +0200

    add init function

--- EAjaxUpload.php
+++ EAjaxUpload.php
@@ -22,7 +22,13 @@ class EAjaxUpload extends CWidget
 	public $config = array();
 	public $css;
 
-	public function run()
+	/**
+	 * Initializes the widget.
+	 * This method is called by {@link CBaseController::createWidget}
+	 * and {@link CBaseController::beginWidget} after the widget's
+	 * properties have been initialized.
+	 */
+	public function init()
 	{
 		if (!$this->config['request']['endpoint']) {
 			throw new CException('EAjaxUpload: config["request"]["endpoint"] cannot be empty.');
@@ -35,19 +41,26 @@ class EAjaxUpload extends CWidget
 		if (!$this->config['validation']['sizeLimit']) {
 			throw new CException('EAjaxUpload: config["validation"]["sizeLimit"] cannot be empty.');
 		}
+	}
 
+	/**
+	 * Executes the widget.
+	 * This method is called by {@link CBaseController::endWidget}.
+	 */
+	public function run()
+	{
 		echo '<div id="' . $this->id . '"><noscript><p>Please enable JavaScript to use file uploader.</p></noscript></div>';
 
 		$assets = dirname(__FILE__) . '/assets';
 		$baseUrl = Yii::app()->assetManager->publish($assets);
 
 		$cs = Yii::app()->getClientScript();
-		
+
 		if (YII_DEBUG)
 			$js = $baseUrl . '/jquery.fineuploader-' . self::VERSION . '.js';
 		else
 			$js = $baseUrl . '/jquery.fineuploader-' . self::VERSION . '.min.js';
-		
+
 		$cs->registerScriptFile($js, CClientScript::POS_HEAD);
 
 		$this->css = (!empty($this->css)) ? $this->css : $baseUrl . '/fineuploader-' . self::VERSION . '.css';
