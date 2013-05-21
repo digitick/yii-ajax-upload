145c9a29975389ca5916501267c3c6199699b414
ianaré sévi <ianare.sevi@digitick.com>
2013-05-21 17:10:26 +0200

    Allow not loading JS on AJAX & fixes to validation

--- EAjaxUpload.php
+++ EAjaxUpload.php
@@ -21,6 +21,7 @@ class EAjaxUpload extends CWidget
 	public $params = array();
 	public $config = array();
 	public $css;
+	public static $ajaxJsLoad = true;
 
 	/**
 	 * Initializes the widget.
@@ -30,17 +31,36 @@ class EAjaxUpload extends CWidget
 	 */
 	public function init()
 	{
-		if (!$this->config['request']['endpoint']) {
+		if (!isset($this->config['request']['endpoint'])) {
 			throw new CException('EAjaxUpload: config["request"]["endpoint"] cannot be empty.');
 		}
 
-		if (empty($this->config['validation']['allowedExtensions'])) {
+		if (!isset($this->config['validation']['allowedExtensions']) || empty($this->config['validation']['allowedExtensions'])) {
 			throw new CException('EAjaxUpload: config["validation"]["allowedExtensions"] cannot be empty.');
 		}
 
-		if (!$this->config['validation']['sizeLimit']) {
+		if (!isset($this->config['validation']['sizeLimit'])) {
 			throw new CException('EAjaxUpload: config["validation"]["sizeLimit"] cannot be empty.');
 		}
+	}
+	
+	public static function registerJs(CClientScript $cs)
+	{
+		if (!self::$ajaxJsLoad && Yii::app()->request->isAjaxRequest) {
+	        return;
+	    }
+		
+		$assets = dirname(__FILE__) . '/assets';
+		$baseUrl = Yii::app()->assetManager->publish($assets);
+		
+		if (YII_DEBUG)
+			$js = $baseUrl . '/jquery.fineuploader-' . self::VERSION . '.js';
+		else
+			$js = $baseUrl . '/jquery.fineuploader-' . self::VERSION . '.min.js';
+
+		$cs->registerScriptFile($js, CClientScript::POS_HEAD);
+		
+		return $baseUrl;
 	}
 
 	/**
@@ -51,17 +71,9 @@ class EAjaxUpload extends CWidget
 	{
 		echo '<div id="' . $this->id . '"><noscript><p>Please enable JavaScript to use file uploader.</p></noscript></div>';
 
-		$assets = dirname(__FILE__) . '/assets';
-		$baseUrl = Yii::app()->assetManager->publish($assets);
-
 		$cs = Yii::app()->getClientScript();
 
-		if (YII_DEBUG)
-			$js = $baseUrl . '/jquery.fineuploader-' . self::VERSION . '.js';
-		else
-			$js = $baseUrl . '/jquery.fineuploader-' . self::VERSION . '.min.js';
-
-		$cs->registerScriptFile($js, CClientScript::POS_HEAD);
+		$baseUrl = self::registerJs($cs);
 
 		$this->css = (!empty($this->css)) ? $this->css : $baseUrl . '/fineuploader-' . self::VERSION . '.css';
 		$cs->registerCssFile($this->css);
@@ -87,7 +99,14 @@ class EAjaxUpload extends CWidget
 		$config = CJavaScript::encode(CMap::mergeArray($configArray, $this->config));
 
 		$script = "var qq_{$this->id} = new qq.FineUploader($config);";
-		$cs->registerScript('FineUploader_' . $this->id, $script, CClientScript::POS_READY);
+		
+		if (!self::$ajaxJsLoad && Yii::app()->request->isAjaxRequest) {
+	        $position = CClientScript::POS_END;
+	    }
+		else {
+			$position = CClientScript::POS_READY;
+		}
+		$cs->registerScript('FineUploader_' . $this->id, $script, $position);
 	}
 
 }
